dedaca41ec98946cd286672033a6c739802606d9
tunnckoCore <mameto_100@mail.bg>
2015-04-11 17:26:39 +0300

    first commits

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * jstransformer-gray-matter <https://github.com/jstransformers/jstransformer-gray-matter>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var matter = require('gray-matter');
---
+++ test/index.js
@@ -0,0 +1,12 @@
+/**
+ * jstransformer-gray-matter <https://github.com/jstransformers/jstransformer-gray-matter>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var test = require('assertit');
+var transformer = require('jstransformer');
+var transform = transformer(require('../index'));
