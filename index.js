190e3357eea16680df34718b7f690d40e9865d92
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-/**
- * jstransformer-gray-matter <https://github.com/jstransformers/jstransformer-gray-matter>
- *
- * Copyright (c) 2015 Charlike Mike Reagent, contributors.
- * Released under the MIT license.
- */
+'use strict'
 
-'use strict';
+var matter = require('gray-matter')
 
-var matter = require('gray-matter');
+exports.name = 'gray-matter'
+exports.outputFormat = 'json'
 
-exports.name = 'gray-matter';
-exports.outputFormat = 'json';
-
-exports.render = function _render(str, options) {
-  var result = matter(str, options);
-  return JSON.stringify(result);
-};
+exports.render = function (str, options) {
+  var result = matter(str, options)
+  return JSON.stringify(result)
+}
